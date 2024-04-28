32d3c3c46968cfbd77c56061777f13cbf7fd0273
PalakAgrawal020 <palakagrawal020@gmail.com>
2024-04-28 20:09:06 +0530

    getUser controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,41 @@
 import { User } from "../models/user.model.js";
 
 export const createUser = async (req, res) => {
+    const { username, email, password } = req.body
+
+    const existedUser = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if (existedUser) {
+        res.status(500).json({error : 'user already exists'})
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password,
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password"
+    )
+
+    if (!createdUser) {
+        res.status(500).json({error : 'something went wrong while registering the user'})
+    }
+
+    return res.status(201).json(
+        {user: createdUser}
+    )
+
+}
+
+
+export const getUser = async (req, res) => {
     try {
-        const { username, email, password } = req.body;
-        const user = new User({ username, email, password });
-        await user.save();
-        res.status(201).json(user);
+        res.status(201).json({'msg' : 'hello'})
     } catch (error) {
-        res.status(500).json({'error' : 'User not created'})
+        res.status(500).json({'error' : error})
     }
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, getUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post('/create-user', createUser)
+router.post('/get-user', getUser)
 
 export default router;
